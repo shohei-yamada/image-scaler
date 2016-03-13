579460ee93db4373a42bdf9fc0e1b21a67dc0666
shohei <ymd.mobi@gmail.com>
2016-03-13 21:15:02 +0900

    画像の横縦幅を変更できるメソッドを作った。

--- src/main/java/com/ctrltf/image_scaler/App.java
+++ src/main/java/com/ctrltf/image_scaler/App.java
@@ -1,9 +1,9 @@
 package com.ctrltf.image_scaler;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -12,16 +12,15 @@ import org.apache.commons.io.IOUtils;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	FileInputStream fis = null;
-    	try {
+	public static void main( String[] args )
+	{
+		FileInputStream fis = null;
+		try {
 			fis = new FileInputStream("image/sample.jpg");
 			BufferedImage bimage = ImageIO.read(fis);
-			bimage = ImageScaler.scalingWidth(bimage, 200);
-			FileOutputStream fout = new FileOutputStream("image/result.jpg");
-			ImageIO.write(bimage, "jpeg", fout);
-			
+			bimage = ImageScaler.changeSize(bimage, 1000, 500);
+			ImageIO.write(bimage, "jpg", new File("image/result.jpg"));
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -29,7 +28,7 @@ public class App
 		} finally {
 			IOUtils.closeQuietly(fis);
 		}
-    	
-    	
-    }
+
+
+	}
 }
